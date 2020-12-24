5caf9d286d97137536c5b8c746fdd87c19082191
erkanonat <erkan.onat90@gmail.com>
2020-12-24 19:00:22 +0300

    second commit

---
+++ src/main/java/com/example/demo/service/K8sClientExample.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class K8sClientExample {
+
+    public void listDeployments() {
+
+        try {
+
+            Config config = new ConfigBuilder().withMasterUrl("https://mymaster.com").build();
+            KubernetesClient client = new DefaultKubernetesClient(config);
+
+
+        }catch (Exception ex) {
+             log.error(ex.toString());
+        }
+
+    }
+
+}
